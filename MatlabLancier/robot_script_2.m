5202ddc8adacda1a7719937aebae48a2b67e73b1
TemperTamperer <50406742+TemperTamperer@users.noreply.github.com>
2025-03-15 18:22:14 +0100

    Added matlab and simulink files to project folder

---
+++ MatlabLancier/robot_script_2.m
@@ -0,0 +1,36 @@
+%% init
+close all
+clear
+%%
+% motor inerti
+ankerRadius = 0.0115;
+ankerLength = 0.025;
+ankerVolumen = ankerRadius^2*pi*ankerLength
+ankerMass = 8000 * ankerVolumen
+ankerInerti = 0.5 * ankerMass * ankerRadius^2
+%% make control time series
+% format: 
+% [time(sec) , longitudinal velocity (m/s), rotation velocity (rad/sec)]
+vel = [0, 0, 0; ...
+       0.1, 1, 0;
+       0.2, 2, 0; 
+       0.3, 3.5, 0; 
+       0.4, 5, 0; 
+       0.5, 7.5, 0;
+       0.6, 5, 0;
+       0.8, 3.5, 0;
+       1.0, 7, 0;
+       1.2, 1, 0;...
+       1.5, 0,   0; ...
+       ]
+% [time(sec) , longitudinal velocity (m/s) , heading (rad)]
+vel2 = [0, 0, 0; ...
+       0.1, 0.7, 0; ...
+       0.7, 0.7, pi/2; ...
+       1.5, 0.5, pi/2; ...
+       2.0, 0.5, pi/2; ...
+       2.5, 0,   pi/2; ...
+       ]
+
+vel3 = [0, 0, 0; ...
+       ]
